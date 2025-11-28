a82d7f399d9cfbe01502ffff73133e60ad3164a8
Karlaleticiaavila <Kbliaass@gmail.com>
2025-11-27 17:01:37 -0800

    exercise 3 jquery and ajax

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 $(function() {
-  // your code here
-})
+
+    //  ACCORDION
+    $(".accordion-header").click(function() {
+        $(".accordion-content").slideUp();
+        $(this).next(".accordion-content").slideDown();
+    });
+
+    //  AJAX - TO-DO ITEMS
+    $(".todos button").click(function() {
+        $.ajax({
+            url: "https://dummyjson.com/todos",
+            method: "GET",
+            success: function(data) {
+                const ul = $(".todos ul");
+                ul.empty();
+                data.todos.forEach(todo => {
+                    ul.append('<li>${todo.todo}</li>');
+                });
+            },
+            error: function() {
+                alert("Hubo un error al cargar los To-Do items.");
+            }
+        });
+    });
+
+});
+
